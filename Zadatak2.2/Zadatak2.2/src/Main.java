fa3540bb3ff192f6e24fa8f4b0c682ce05f2f943
Boze Vukman <bozevukman7@gmail.com>
2026-03-26 10:25:11 +0100

    Dodan Zadatak2.2

---
+++ Zadatak2.2/Zadatak2.2/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        reversewords();
+    }
+
+    public static void reversewords(){
+
+        System.out.println("Unesi rečenicu: ");
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        String[] words = input.trim().split("\\s+");
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = words.length -1; i >= 0; i--){
+            stringBuilder.append(words[i]);
+
+            if (i > 0){
+                stringBuilder.append(" ");
+            }
+        }
+
+        System.out.println(stringBuilder);
+    }
+}
